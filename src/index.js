8598d486abbb61b2fa1ae0d3887e4f298900d3f8
Trevor Glasgow <Trevor.T.Glasgow.20@dartmouth.edu>
2019-04-11 16:39:28 -0400

    Finished Version1 of Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import $ from 'jquery';
+import './style.scss';
+
 console.log('starting up!');
+let num = 0;
+
+setInterval(() => { $('#main').html(`You've been on this page for ${num += 1} seconds.`); }, 1000);
