8bdbc8db0a9f0231179762e2f73fbf5ee27ac388
cameoapp <benjamin.eisner.20@dartmouth.edu>
2019-05-08 21:38:55 -0400

    WORKSHOP-STARTER

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,4 @@
-import ApolloClient from 'apollo-boost';
-import { GetRepos, AddStar, RemoveStar } from './operations';
-/* const ROOT_URL = graphql api key */
+// import your npm packages
 
 // keys for actiontypes
 export const ActionTypes = {
@@ -10,65 +8,24 @@ export const ActionTypes = {
 };
 
 const GITHUB_API = 'https://api.github.com/graphql';
-const API_KEY = '6321858ac97795a931a08c22f9ba3b6ee3e20cea';
+const API_KEY = 'YOUR_KEY';
 
-const client = new ApolloClient({
-  uri: GITHUB_API,
-  headers: { authorization: `bearer ${API_KEY}` },
-});
-
+// initialize ApolloClient here
 
 export function fetchRepos(query) {
   return (dispatch) => {
-    client.query({
-      query: GetRepos,
-      variables: {
-        queryString: query,
-      },
-      fetchPolicy: 'no-cache',
-    })
-      .then((response) => {
-        const repos = response.data.search.edges[0].node.repositories.edges.map(repo => repo.node);
-        dispatch({ type: ActionTypes.FETCH_REPOS, payload: repos });
-      })
-      .catch((error) => {
-        dispatch({ type: ActionTypes.ERROR_SET, error });
-      });
+    // fetchRepos query
   };
 }
 
 export function addStar(repoID, searchTerm) {
   return (dispatch) => {
-    client.mutate({
-      mutation: AddStar,
-      variables: {
-        id: repoID,
-      },
-      fetchPolicy: 'no-cache',
-    })
-      .then((res) => {
-        dispatch(fetchRepos(searchTerm));
-      })
-      .catch((error) => {
-        dispatch({ type: ActionTypes.ERROR_SET, error });
-      });
+    // addStar mutation
   };
 }
 
 export function removeStar(repoID, searchTerm) {
   return (dispatch) => {
-    client.mutate({
-      mutation: RemoveStar,
-      variables: {
-        id: repoID,
-      },
-      fetchPolicy: 'no-cache',
-    })
-      .then(async (res) => {
-        await dispatch(fetchRepos(searchTerm));
-      })
-      .catch((error) => {
-        dispatch({ type: ActionTypes.ERROR_SET, error });
-      });
+    // removeStar mutation
   };
 }
